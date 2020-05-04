a6657e98c16b84b7b5c71f97e7c2a1af335777f5
Lario dos Santos Diniz <lariodiniz@gmail.com>
2020-05-04 10:45:38 -0300

    Adicionado sideBarMobile

--- frontend/src/common/templates/base/index.jsx
+++ frontend/src/common/templates/base/index.jsx
@@ -2,18 +2,30 @@ import React from 'react'
 
 import Header from '../header'
 import Sidebar from '../sidebar'
+import SidebarMobile from '../sidebarMobile'
+
+import IF from '../../utils/if'
 
 import './base.css';
 
-export default props => (
-
+export default props => {
+    
+    let isMobile = window.innerWidth <= 575;
+    
+    return(
     <React.Fragment>
 
         <Header disease={props.disease} title={props.title}/>
         <div className="container-fluid">
             <div className="row">
-                <Sidebar/>
+                <IF test={!isMobile}>
+                     <Sidebar/>
+                </IF>
+                
                 <main role="main" className="col-12 col-sm-10 main">
+                    <IF test={isMobile}>
+                        <SidebarMobile />
+                    </IF>
                     {props.children}
                 </main>
             </div>
@@ -21,6 +33,6 @@ export default props => (
     </React.Fragment>
 
 )
-
+}
 
 
---
+++ frontend/src/common/templates/sidebarMobile/index.jsx
@@ -0,0 +1,26 @@
+import React, { Component }  from 'react'
+import { Link } from 'react-router-dom'
+
+import './sidebarMobile.css';
+
+
+
+
+
+class sidebarMobile extends Component {
+
+    render(){
+        return (
+            <nav className="navbar navbar-light bg-light">
+              
+                <Link className="nav-item nav-link sidebarMobile-nav-item-a" to="/">Index</Link>
+                <Link className="nav-item nav-link sidebarMobile-nav-item-a" to="/dados">Dados Diarios</Link>
+                <a className="nav-item nav-link sidebarMobile-nav-item-a" target="_blank"  rel="noopener noreferrer" href="http://lariodiniz.com.br/">Desenvolvedor</a>
+              
+            </nav>
+        )
+    }
+
+}
+
+export default sidebarMobile
